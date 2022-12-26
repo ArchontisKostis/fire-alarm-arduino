7bcd148afec4021c205bfb51a6a765d33ac3aa36
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2022-12-26 21:16:23 +0200

    Created DigitDisplay
    Moved the implementation of the digit display on a different class

---
+++ DigitDisplay.cpp
@@ -0,0 +1,26 @@
+#include "HardwareSerial.h"
+#include <Arduino.h>
+#include "SevSeg.h"
+
+class DigitDisplay {
+  private:
+    SevSeg display;
+
+  public:
+    DigitDisplay(byte hardwareConfig, byte numberOfDigits, byte digitPins[], byte segmentPins[], bool resistorsOnSegments = true) {
+      display.begin(hardwareConfig, numberOfDigits, digitPins, segmentPins, resistorsOnSegments);
+      display.setBrightness(100);
+    }
+
+    void setNumber(int numberToShow) {
+      display.setNumber(numberToShow);
+    }
+
+    void setChars(char *characterToShow) {
+      display.setChars(characterToShow);
+    }
+
+    void refresh() {
+      display.refreshDisplay();
+    }
+};
--- fire-alarm-oop.ino
+++ fire-alarm-oop.ino
@@ -1,47 +1,44 @@
 #include "FlameSensor.cpp"
 #include "Potentiometer.cpp"
 #include "Alarm.cpp"
-#include "SevSeg.h"
+#include "DigitDisplay.cpp"
 
 #define BUZZER_PIN 11
 #define RED_LED_PIN 12
 #define BLUE_LED_PIN 13
+#define FLAME_SENSOR_PIN A1
+#define POTENTIOMETER_PIN A0
 
-SevSeg sevseg;  
-FlameSensor flameSensor(A1);
-Potentiometer potentiometer(A0);
+// --- DISPLAY INITIALIZATION DATA ---
+byte numDigits = 1;
+byte digitPins[] = {};
+byte segmentPins[] = {6, 5, 2, 3, 4, 7, 8, 9};
+bool resistorsOnSegments = true;
+// ------------------------------------
+
+FlameSensor flameSensor(FLAME_SENSOR_PIN);
+Potentiometer potentiometer(POTENTIOMETER_PIN);
 Alarm alarm(BUZZER_PIN, RED_LED_PIN, BLUE_LED_PIN);
+DigitDisplay digitDisplay(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
 
 
 void setup() {
   Serial.begin(9600);
-  initializeDigitDisplay();
 }
 
 void loop() {
-  int potentiometerValue = potentiometer.getValue();
-  int potentiometerDisplayValue = potentiometerValue / 113;
+  int sensitivity = potentiometer.getValue();
+  int sensitivityDisplayValue = sensitivity / 113;
 
-  flameSensor.setSensitivity(potentiometerValue);
+  flameSensor.setSensitivity(sensitivity);
 
   if(flameSensor.fireDetected()) {
-    sevseg.setChars("F");
+    digitDisplay.setChars("F");
     alarm.setOnAlarm();
   }
   else {
-    sevseg.setNumber(potentiometerDisplayValue);
+    digitDisplay.setNumber(potentiometerDisplayValue);
     alarm.setOffAlarm();
   }
-  sevseg.refreshDisplay();
-}
-
-void initializeDigitDisplay() {
-  byte numDigits = 1;
-  byte digitPins[] = {};
-  byte segmentPins[] = {6, 5, 2, 3, 4, 7, 8, 9};
-  bool resistorsOnSegments = true;
-
-  byte hardwareConfig = COMMON_CATHODE; 
-  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
-  sevseg.setBrightness(100);
+  digitDisplay.refresh();
 }
